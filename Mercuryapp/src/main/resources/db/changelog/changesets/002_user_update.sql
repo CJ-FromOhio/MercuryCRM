67a74f8ea1cb0ac03eb19bac65744c5baaa2d01c
tropia <lev281010@protonmail.com>
2026-04-04 17:31:11 +0300

    added liquibase user update file, added firstname and lastname for user

--- Mercuryapp/src/main/resources/db/changelog/changesets/002_user_update.sql
+++ Mercuryapp/src/main/resources/db/changelog/changesets/002_user_update.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset tropia:1
+--changeset tropia:5
 ALTER TABLE users
     ADD COLUMN firstname VARCHAR(64) NOT NULL default 'firstname',
     ADD COLUMN lastname VARCHAR(64) NOT NULL default 'lastname'
