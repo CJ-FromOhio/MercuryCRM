ef34048586836a6748c5b829476c6cdae74af329
tropia <lev281010@protonmail.com>
2026-04-04 16:23:33 +0300

    initial commit

---
+++ Mercuryapp/src/main/java/com/tropia/mercuryapp/MercuryappApplication.java
@@ -0,0 +1,13 @@
+package com.tropia.mercuryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercuryappApplication.class, args);
+    }
+
+}
---
+++ Mercuryapp/src/test/java/com/tropia/mercuryapp/MercuryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tropia.mercuryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercuryappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
